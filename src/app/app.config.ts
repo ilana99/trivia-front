cf7f5e788176fc8a525cdafbe622dac22ed764f2
ilana99 <ilanagastou@gmail.com>
2025-08-24 16:51:17 +0200

    angular update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { credentialsInterceptor } from './credentials.interceptor';
 
 export const appConfig: ApplicationConfig = {
